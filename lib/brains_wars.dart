4bc8aeb10d91ec10635b497cd9b59c084726473c
Ayush783 <ayushc45xxx@gmail.com>
2021-06-28 22:41:57 +0530

    started app

---
+++ lib/brains_wars.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BrainWars extends StatelessWidget {
+  const BrainWars({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
