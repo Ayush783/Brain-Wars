c9fed1acab013b2537141d12bf4d9f16b3577272
Ayush783 <ayushc45xxx@gmail.com>
2021-10-20 18:16:53 +0530

    added a profile placeholder on home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,8 +5,58 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('ho gya login'),
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: size.height * 0.1,
+                        width: size.height * 0.1,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(1),
+                          borderRadius: BorderRadius.circular(100),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 12,
+                              color: Color(0xff000000).withOpacity(0.5),
+                              offset: Offset(2, 2),
+                              spreadRadius: 1,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Ayushb58'),
+                            Chip(
+                              label: Text('Quiz Master'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.people),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
